2f85e39f0a77d7be59815e07860560cd31f1e3d0
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-08-27 02:53:52 -0700

    Documented summarizeConflicts, removed unnecessary summarizeDeps.

--- R/summarizeConflicts.R
+++ R/summarizeConflicts.R
@@ -1,53 +1,46 @@
+#' Summarize conflicting dependencies
+#'
+#' Summarize conflicts between dependencies with similar but not identical properties.
+#'
+#' @param full List produced by \code{\link{queryAllDependencies}}.
+#' 
+#' @return List containing lists of data frames.
+#'
+#' This includes \code{name}, a named list of data frames where each entry corresponds to a set of conflicting dependencies.
+#' Each entry is named after a particular project's name and contains a data frame of conflicting projects with differences in properties other than the name.
+#'
+#' The same applies for \code{git.repository}, where conflicting projects have the same Git repository but differences in other properties;
+#' and \code{url}, for projects with the same URL but differences elsewhere.
+#' 
+#' @examples
+#' df <- data.frame(
+#'     name = "scran", 
+#'     git.repository = "https://github.com/LTLA/libscran",
+#'     git.tag = "ae74e0345303a2d2c6e70d599d72c0e02d346fb6",
+#'     url = NA_character_,
+#'     url.hash = NA_character_
+#' )
+#' 
+#' path <- fetchDependencies(df)
+#' out <- queryAllDependencies(path)
+#' summarizeConflicts(out)
+#' 
 #' @export
 summarizeConflicts <- function(full) {
     output <- list()
 
     for (prop in c("name", "git.repository", "url")) {
-        splitted <- split(seq_len(nrow(full)), full[[prop]])
+        splitted <- split(seq_len(nrow(full$dependencies)), full$dependencies[[prop]])
         failures <- list()
 
         for (x in names(splitted)) {
             current <- splitted[[x]]
-            ids <- full$`_id`[current]
-            by.id <- split(full[current,c("_path", "_parent")], ids)
-
-            if (length(by.id) > 1) {
-                failures[[x]] <- lapply(by.id, function(y) {
-                    rownames(y) <- NULL
-                    y
-                })
+            if (length(current) > 1) {
+                failures[[x]] <- full$dependencies[current,setdiff(colnames(full$dependencies), prop),drop=FALSE]
             }
         }
         output[[prop]] <- failures
     }
 
     output
-}
-
-#' @export
-summarizeDependencies <- function(full) {
-    unique.ids <- unique(full$`_id`)
-    col.details <- colnames(full)
-    col.details <- col.details[!grepl("^_", col.details)]
-    unique.overview <- full[match(unique.ids, full$`_id`),col.details]
-    rownames(unique.overview) <- NULL
-
-    # Creating paths to the root for each entry.
-    origins <- list()
-    for (u in unique.ids) {
-        flock <- data.frame(`_id` = character(0), `_path` = character(0), `_parent` = character(0), check.names=FALSE)
-        current.nodes <- u
-
-        while (length(current.nodes)) {
-            kept <- full[full$`_id` %in% current.nodes, colnames(flock)]
-            flock <- rbind(flock, kept)
-            current.nodes <- kept$`_parent`
-            current.nodes <- current.nodes[!(current.nodes %in% flock$`_id`)]
-        }
-
-        rownames(flock) <- NULL
-        origins[[u]] <- flock
-    }
-    
-    list(dependencies = unique.overview, origins = origins)
 }
